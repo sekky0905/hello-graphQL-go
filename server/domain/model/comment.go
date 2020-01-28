df194ddafc3e0f0bbbb45fb1d590e6d94d2fe861
sekky0905 <ks.dev.sekky0905@gmail.com>
2020-01-28 23:33:21 +0900

    add new function for comment

--- server/domain/model/comment.go
+++ server/domain/model/comment.go
@@ -10,3 +10,14 @@ type Comment struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+// NewComment は、コメントを表す。
+func NewComment(ID string, content string, postedBy User) *Comment {
+	return &Comment{
+		ID:        ID,
+		Content:   content,
+		PostedBy:  postedBy,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
