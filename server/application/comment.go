1d84bd804d973d2aa620e9729072938f139a6fef
sekky0905 <ks.dev.sekky0905@gmail.com>
2020-01-29 11:11:49 +0900

    refactoring

--- server/application/comment.go
+++ server/application/comment.go
@@ -3,6 +3,7 @@ package application
 import (
 	"github.com/sekky0905/hello-graphQL-go/server/domain/model"
 	"github.com/sekky0905/hello-graphQL-go/server/repository"
+	"golang.org/x/xerrors"
 )
 
 // CommentApplicationService は、Comment の ApplicationService。
@@ -11,9 +12,12 @@ type CommentApplicationService struct {
 }
 
 // CreateComment は、Comment を作成する。
-func (s CommentApplicationService) CreateComment(chatRoomID, id, name string, user model.User) *model.Comment {
-	comment := model.NewComment(id, name, user)
+func (s CommentApplicationService) CreateComment(userID, chatRoomID, content string) (*model.Comment, error) {
+	comment, err := model.NewComment(userID, chatRoomID, content)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to generate comment: %w", err)
+	}
 	// 普通だと諸々の処理を行う。
 	s.Repo.AddComment(chatRoomID, comment)
-	return comment
+	return comment, nil
 }
--- server/domain/model/comment.go
+++ server/domain/model/comment.go
@@ -1,23 +1,36 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/xerrors"
+)
 
 // Comment は、コメントを表す。
 type Comment struct {
-	ID        string
-	Content   string
-	PostedBy  User
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID         string
+	UserID     string
+	ChatRoomID string
+	Content    string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
 
 // NewComment は、コメントを表す。
-func NewComment(ID string, content string, postedBy User) *Comment {
-	return &Comment{
-		ID:        ID,
-		Content:   content,
-		PostedBy:  postedBy,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+func NewComment(userID, chatRoomID, content string) (*Comment, error) {
+	uid, err := uuid.NewUUID()
+	if err != nil {
+		return nil, xerrors.Errorf("failed to generate uuid: %w", err)
 	}
+
+	return &Comment{
+		ID:         fmt.Sprintf("user_%s", uid.String()),
+		UserID:     userID,
+		ChatRoomID: chatRoomID,
+		Content:    content,
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}, nil
 }
