38b866754e1e0c911ff34dfbb754d4ba775149aa
sekky0905 <ks.dev.sekky0905@gmail.com>
2020-02-04 17:34:17 +0900

    add GetCommentListByUserID to CommentRepository

--- server/repository/comment.go
+++ server/repository/comment.go
@@ -28,10 +28,34 @@ func newCommentDTOFromComment(chatRoomID string, comment *model.Comment) *Commen
 	}
 }
 
+// newCommentFromCommentDTO は、CommentDTO から domain model の Comment を作成する。
+func newCommentFromCommentDTO(dto *CommentDTO) *model.Comment {
+	return &model.Comment{
+		ID:         dto.ID,
+		UserID:     dto.UserID,
+		ChatRoomID: dto.ChatRoomID,
+		Content:    dto.Content,
+		CreatedAt:  dto.CreatedAt,
+		UpdatedAt:  dto.UpdatedAt,
+	}
+}
+
 // CommentRepository は、Comment の Repository。
 // 練習用の Project なので、Interface 等は定義しない。
 type CommentRepository struct {
 	MockDB []*CommentDTO
+}
+
+// GetCommentListByUserID は、指定された UserID を持つ Comment の一覧を取得する。
+func (r *CommentRepository) GetCommentListByUserID(userID string) []*model.Comment {
+	var comments []*model.Comment
+	for _, dto := range r.MockDB {
+		if dto.UserID == userID {
+			comments = append(comments, newCommentFromCommentDTO(dto))
+		}
+	}
+
+	return comments
 }
 
 // AddComment は、Comment を追加する。
